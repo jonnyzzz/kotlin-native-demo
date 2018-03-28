9ea5b36a05abd2f2c03ccf89cfb464ba2f58458b
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-03-28 15:28:43 +0200

    implement trivial entry point for now

---
+++ in.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+KONAN_VERSION=0.6.2
+export PATH=$(cd ~/kotlin-native/kotlin-native-macos-$KONAN_VERSION/bin && pwd):$PATH
+
+ACTUAL_VERSION=$(kotlinc -version 2>/dev/null)
+
+if [[ ! "$ACTUAL_VERSION" =~ "$KONAN_VERSION" ]] ; then 
+  echo "Failed to setup Kotlin/Native of version $KONAN_VERSION. Have $ACTUAL_VERSION" >&2
+  sleep 10
+fi
+
+
+
