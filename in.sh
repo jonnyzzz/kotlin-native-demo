ac565e0a52e40b2f4cee0e013460079606d55057
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-05-16 14:53:19 +0200

    v0.7

--- in.sh
+++ in.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-KONAN_VERSION=0.6.2
+KONAN_VERSION=0.7
 export PATH=$(cd ~/kotlin-native/kotlin-native-macos-$KONAN_VERSION/bin && pwd):$PATH
 
 ACTUAL_VERSION=$(kotlinc -version 2>/dev/null)
