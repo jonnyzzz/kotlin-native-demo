4706e20f365b2a47eb87d7eb9b3affa421f51bef
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2018-07-10 14:01:25 +0200

    support linux

--- in.sh
+++ in.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
 KONAN_VERSION=0.8
-export PATH=$(cd ~/kotlin-native/kotlin-native-macos-$KONAN_VERSION/bin && pwd):$PATH
+
+KONAN_INFIX=
+
+### from https://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linux
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     KONAN_INFIX=linux;;
+    Darwin*)    KONAN_INFIX=macos;;
+    CYGWIN*)    echo "Unsupported OS" ; exit 22;;
+    MINGW*)     echo "Unsupported OS" ; exit 22;;
+    *)          echo "Unknown OS"; exit 22;;
+esac
+
+export PATH=$(cd ~/kotlin-native/kotlin-native-$KONAN_INFIX-$KONAN_VERSION/bin && pwd):$PATH
 
 ACTUAL_VERSION=$(kotlinc -version 2>/dev/null)
 
@@ -10,5 +24,6 @@ if [[ ! "$ACTUAL_VERSION" =~ "$KONAN_VERSION" ]] ; then
   sleep 10
 fi
 
-
+# make sure we have all dependencies here
+kotlinc --check_dependencies
 
