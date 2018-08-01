6047cdddddc10ae00929f93650917b55fc68b38e
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2018-08-02 01:31:18 +0200

    more helper scripts

--- dynamic-lib/in.sh
+++ dynamic-lib/in.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+set -x -u -e
+
 kotlinc lib.kt -produce dynamic -output demo
 
-gcc main.c libdemo.dylib
+if [ -f libdemo.dylib ]; then
+  gcc main.c libdemo.dylib
+elif [ -f libdemo.so ]; then
+  gcc main.c libdemo.so
+fi
+
+LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH ./a.out
 
 
