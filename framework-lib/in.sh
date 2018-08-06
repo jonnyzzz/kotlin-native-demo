e98e0f9fb96d0fb9afcad1f3888e3b3f9dce37b3
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-08-06 17:30:35 +0200

    compile for mac, iOS and iOS emulator

--- framework-lib/in.sh
+++ framework-lib/in.sh
@@ -2,7 +2,12 @@
 
 set -x -u -e
 
-kotlinc lib.kt -produce framework -output demo
+
+kotlinc lib.kt -produce framework -output macOS/Demo
+
+kotlinc lib.kt -produce framework -target ios_arm64 -output iOS/Demo
+kotlinc lib.kt -produce framework -target ios_x64 -output iOS_emu/Demo
+
 
 
 
