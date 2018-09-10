ec10074620e234362361c13a40a149917a985957
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-09-10 15:48:13 +0300

    libcurl example

---
+++ curl/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e -x -u
+
+
+konanc main.kt -library build/c_interop/libcurl
+
+
+
+
---
+++ curl/lib.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e -x -u
+
+cinterop -def libcurl.def -o build/c_interop/libcurl
+
+
