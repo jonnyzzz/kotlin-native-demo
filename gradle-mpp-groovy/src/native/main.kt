2a3b1d09e1d38205278b83058c31b42fdc770004
Eugene Petrenko <eugene.petrenko@gmail.com>
2019-01-14 22:26:35 +0100

    get example live

--- gradle-mpp-groovy/src/native/main.kt
+++ gradle-mpp-groovy/src/native/main.kt
@@ -1,11 +1,21 @@
 import file.foo
+import kotlinx.cinterop.IntVar
+import kotlinx.cinterop.alloc
 import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.ptr
+import kotlinx.cinterop.value
 
 fun main(args: Array<String>) {
   println("Hello!")
 
   memScoped {
-    foo(null)
+    val q = alloc<IntVar>()
+    q.value = 123
+
+    val z = foo(q.ptr)
+
+    println(z)
+    println(q.value)
   }
 }
 
